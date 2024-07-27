c23b4b89e09a17d7bf054d4a9c33353e91f81704
WenLu03412 <wenlu@embux.com>
2024-07-27 11:22:14 +0800

    modify

---
+++ 20240727/lesson_20240727_1.py
@@ -0,0 +1,12 @@
+print(100,12,10,11,sep="/")
+print(10,11,10,11,sep="/",end=".")
+print()
+
+name = '123'
+chinese = 78
+english = 89.0
+math = 65.0
+print(name,chinese,english,math)
+print(type(name))
+print(type(chinese))
+print(type(english))
