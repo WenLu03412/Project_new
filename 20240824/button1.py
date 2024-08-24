b23c0506d92418b46f8182c7583c8c25f58eaaa8
WenLu03412 <wenlu@embux.com>
2024-08-24 10:25:53 +0800

    origin

---
+++ 20240824/button1.py
@@ -0,0 +1,35 @@
+import signal
+from gpiozero import Button,LED
+from datetime import datetime
+import paho.mqtt.publish as publish
+
+
+
+def user_release():
+    print("使用者按下放開")
+    led.toggle()
+    now = datetime.now()
+    now_str = now.strftime('%Y-%m-%d %H:%M:%S')
+    print(now_str)
+    if led.is_lit:
+        message = f'''{{
+            "status":true,
+            "date":{now_str},
+            "topic":"myraspberry/led"
+        }}'''
+        print(message)
+        publish.single(topic='myraspberry/led',payload=message,hostname='127.0.0.1',qos=2,auth={'username':os.environ['MQTT_USERNAME'],'password':os.environ['MQTT_PASSWORD']})
+    else:
+        message = f'''{{
+            "status":false,
+            "date":{now_str},
+            "topic":"myraspberry/led"
+        }}''' 
+        print(message)
+        publish.single(topic='myraspberry/led',payload=message,hostname='127.0.0.1',qos=2,auth={'username':os.environ['MQTT_USERNAME'],'password':os.environ['MQTT_PASSWORD']})
+
+if __name__ == '__main__':
+    button = Button(pin=18)
+    button.when_released = user_release
+    led = LED(pin=25) 
+    signal.pause()
